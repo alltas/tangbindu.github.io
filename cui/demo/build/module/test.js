9954f64ec9ef05a6ebd9cba07fc9e007eee71e3c
tangbin <122281326@qq.com>
2016-12-12 17:20:32 +0800

    add cui

---
+++ cui/demo/build/module/test.js
@@ -0,0 +1,2 @@
+var dr=require("./demoResource");
+dr.getWidgetScss();
---
+++ cui/demo/config.rb
@@ -0,0 +1,17 @@
+http_path = "/"
+css_dir = "dist/css"
+html_dir="dist/html"
+sass_dir = "src/sass"
+images_dir = "src/img"
+generated_images_dir = "dist/img"
+
+#Output style 
+#output_style =:expanded
+#output_style =:compact
+#output_style =:nested
+output_style =:compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+#asset_cache_buster :none
